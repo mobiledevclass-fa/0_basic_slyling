b1ea180ae47645a9ff40696e77111d1f11fb63f2
161193 <l11cov@yandex.ru>
2019-09-19 18:17:26 +0300

    Download project

---
+++ script.js
@@ -0,0 +1,78 @@
+function update_col(){
+	var col = document.getElementById("myspan").innerText;
+
+	col = parseInt(col) + 1;
+
+	document.getElementById("myspan").innerHTML = col;
+}
+
+
+function menu_1() {
+
+	var price = document.getElementById("myprice").innerText;
+
+	col = parseInt(price) + 1050;
+
+	document.getElementById("myprice").innerHTML = col;
+
+}
+
+function menu_2() {
+
+	var price = document.getElementById("myprice").innerText;
+
+	col = parseInt(price) + 980;
+
+	document.getElementById("myprice").innerHTML = col;
+
+}
+
+function menu_3() {
+
+	var price = document.getElementById("myprice").innerText;
+
+	col = parseInt(price) + 720;
+
+	document.getElementById("myprice").innerHTML = col;
+
+}
+
+function menu_4() {
+
+	var price = document.getElementById("myprice").innerText;
+
+	col = parseInt(price) + 2100;
+
+	document.getElementById("myprice").innerHTML = col;
+
+}
+
+function menu_5() {
+
+	var price = document.getElementById("myprice").innerText;
+
+	col = parseInt(price) + 999;
+
+	document.getElementById("myprice").innerHTML = col;
+
+}
+
+function menu_6() {
+
+	var price = document.getElementById("myprice").innerText;
+
+	col = parseInt(price) + 1290;
+
+	document.getElementById("myprice").innerHTML = col;
+
+}
+
+function menu_7() {
+
+	var price = document.getElementById("myprice").innerText;
+
+	col = parseInt(price) + 500;
+
+	document.getElementById("myprice").innerHTML = col;
+
+}
